a49423442ca5582d02411b5dcffd6bfdbcebfd81
Paulo Marques <paulomarques.rs1@gmail.com>
2024-11-18 14:25:32 -0300

    Add implementation of sync packages and update function calls in main.go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import t1fundamentos "github.com/soupaulodev/mastering-backend-with-golang/topics/t1-fundamentos"
+
+func main() {
+	t1fundamentos.TestSintaxe()
+	t1fundamentos.TestStruct()
+	t1fundamentos.TestIController()
+	t1fundamentos.TestConcorrenciaEParalelismo()
+	t1fundamentos.TestGourotines()
+	t1fundamentos.TestChannels()
+	t1fundamentos.TestSyncPackages()
+}
